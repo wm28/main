d53d4cb3243fac7b77a63638a3236d37e932b3f8
aaryamNUS <aaryamps1@gmail.com>
2018-09-30 12:27:11 +0800

    Removed unused import statement

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -5,7 +5,6 @@ import java.util.logging.Logger;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
