533ce8e466b13330ad503ad6251c71cf91a1a908
SarahTaaherBonna <sarahtb25@gmail.com>
2018-10-02 12:20:40 +0800

    Finally got the program to accept filter command

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 
 import seedu.address.commons.core.Messages;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.person.ContainsKeywordsPredicate;
