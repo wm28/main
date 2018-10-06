d32f099afb87c51fc63687cc2b3168d85c32a5dd
aaryamNUS <aaryamps1@gmail.com>
2018-10-06 20:40:34 +0800

    Fixed tag labels and changed name of application

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -18,13 +18,14 @@ public class PersonCard extends UiPart<Region> {
     /**
      * The following string array represents different tag colours associated with each guest in the list.
      * Each colour represents a charecteristic of the guest, as summarised below:
-     * Orange - Absent, Yellow - Present, Green - Vegetarian, Light Blue - VIP, Red - Bride or Groom,
-     * White - Guest Speaker, Black - Not Paid, Purple - Paid
+     * Orange - Absent, Yellow - Present, Light Blue - VIP,
+     * White - Guest Speaker, Black - Not Paid, Purple - Paid,
+     * Default - a tag that is not supported by the application specifications
      *
      * Note: This code was adapted from the example implementation provide by @yamgent from SE-EDU
      */
-    private static final String[] TAG_COLORS = {"orange", "yellow", "green", "lightblue",
-                                                "red", "white", "black", "purple"};
+    private static final String[] TAG_COLORS = {"orange", "yellow", "lightblue",
+                                                "white", "black", "purple", "default"};
     //@@author
 
     /**
@@ -73,8 +74,22 @@ public class PersonCard extends UiPart<Region> {
          * Using the hashcode of the tag name ensures the color of the tag remains consistent
          * during different iterations of the code by generating a random color
          */
-
-        return TAG_COLORS[Math.abs(tagName.hashCode()) % TAG_COLORS.length];
+        switch (tagName.trim().toLowerCase().replaceAll("\\s+","")) {
+        case "absent":
+            return TAG_COLORS[0];
+        case "present":
+            return TAG_COLORS[1];
+        case "vip":
+            return TAG_COLORS[2];
+        case "guestspeaker":
+            return TAG_COLORS[3];
+        case "notpaid":
+            return TAG_COLORS[4];
+        case "paid":
+                return TAG_COLORS[5];
+        default:
+            return "default";
+        }
     }
 
     /**
