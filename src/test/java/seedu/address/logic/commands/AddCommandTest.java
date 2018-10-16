f92ef4fcb8cc86553de6877911faca1c9ffdfde6
SANDHYA\sandh <e0248114@u.nus.edu>
2018-10-16 23:43:11 +0800

    testing edit

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -118,8 +118,14 @@ public class AddCommandTest {
         public boolean hasEvent() {
             throw new AssertionError("This method should not be called.");
         }
+
         @Override
         public void deletePerson(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteEvent() {
             throw new AssertionError("This method should not be called.");
         }
 
@@ -130,6 +136,11 @@ public class AddCommandTest {
 
         @Override
         public ObservableList<Person> getFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Event getEventDetails() {
             throw new AssertionError("This method should not be called.");
         }
 
