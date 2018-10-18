20b49b355ac69a16c4c740b745370ee490ef1a82
wm28 <e0176337@u.nus.edu>
2018-10-18 18:12:22 +0800

    Removed static utility class CsvUtil to improve testability of Import/Export commands

---
+++ src/main/java/seedu/address/logic/converters/fileformats/SupportedFile.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.converters.fileformats;
+
+import java.io.IOException;
+import java.util.List;
+
+//@@author wm28
+/**
+ * Represents a supported file that can read and write AdaptedPersons
+ */
+public interface SupportedFile {
+
+    /**
+     * Read {@code} AdaptedPerson of all types from file.
+     */
+    List<AdaptedPerson> readAdaptedPersons() throws IOException;
+
+    /**
+     * Write {@code} AdaptedPerson of all types to file
+     */
+    void writeAdaptedPersons(List<AdaptedPerson> adaptedPersons) throws IOException;
+
+    /**
+     * Returns the file format the PersonConverter supports
+     */
+    SupportedFileFormat getSupportedFileFormat();
+
+    /**
+     * Returns the file name of the file
+     */
+    String getFileName();
+}
+//@@author
