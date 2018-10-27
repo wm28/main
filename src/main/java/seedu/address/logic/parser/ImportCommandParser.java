92c0e3bb597bb0aeaea55386537cda876798e05b
wm28 <e0176337@u.nus.edu>
2018-10-27 19:47:53 +0800

    Added unit test for ImportCommandParser

--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -4,13 +4,19 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_FILE_EXTENSION;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_FILE_PATH;
 
+import java.util.Optional;
+
 import seedu.address.commons.util.FileUtil;
 import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.converters.CsvConverter;
+import seedu.address.logic.converters.fileformats.SupportedFileFormat;
 import seedu.address.logic.converters.fileformats.csv.CsvFile;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+
+
 //@@author wm28
+
 /**
  * Parses input arguments and creates a new ImportCommand object
  */
@@ -19,20 +25,44 @@ public class ImportCommandParser implements Parser<ImportCommand> {
     /**
      * Parses the given argument {@code String} in the context of the ImportCommand
      * and returns an ImportCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public ImportCommand parse(String arg) throws ParseException {
         String trimmedArg = arg.trim();
+        Optional<SupportedFileFormat> fileFormat;
 
         if (trimmedArg.isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE));
         } else if (!FileUtil.isValidPath(trimmedArg)) {
             throw new ParseException(String.format(MESSAGE_INVALID_FILE_PATH, ImportCommand.MESSAGE_USAGE));
-        } else if (!FileUtil.isValidFileExtension(trimmedArg, "csv")) {
-            throw new ParseException(String.format(MESSAGE_INVALID_FILE_EXTENSION, ImportCommand.MESSAGE_USAGE));
         }
 
-        return new ImportCommand(new CsvFile(trimmedArg), new CsvConverter());
+        fileFormat = findSupportedFileFormat(trimmedArg);
+
+        if (fileFormat.isPresent()) {
+            switch (fileFormat.get()) {
+            case CSV:
+                return new ImportCommand(new CsvFile(trimmedArg), new CsvConverter());
+            default:
+                throw new ParseException(String.format(MESSAGE_INVALID_FILE_EXTENSION, ImportCommand.MESSAGE_USAGE));
+            }
+        } else{
+            throw new ParseException(String.format(MESSAGE_INVALID_FILE_EXTENSION, ImportCommand.MESSAGE_USAGE));
+        }
+    }
+
+    /**
+     * Searches SupportedFileFormat for the correct file format
+     */
+    private Optional<SupportedFileFormat> findSupportedFileFormat(String trimmedArg) {
+        Optional<SupportedFileFormat> fileFormat = Optional.empty();
+        for (SupportedFileFormat supportedFileFormat : SupportedFileFormat.values()) {
+            if (FileUtil.isValidFileExtension(trimmedArg, supportedFileFormat.name())) {
+                fileFormat = Optional.of(supportedFileFormat);
+            }
+        }
+        return fileFormat;
     }
 }
 //@@author
---
+++ src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
@@ -0,0 +1,40 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_FILE_EXTENSION;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_FILE_PATH;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.ImportCommand;
+import seedu.address.logic.converters.CsvConverter;
+import seedu.address.logic.converters.fileformats.csv.CsvFile;
+
+public class ImportCommandParserTest {
+
+    private ImportCommandParser parser = new ImportCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "    ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidPath_throwsParseException() {
+        assertParseFailure(parser, "path@.csv", String.format(MESSAGE_INVALID_FILE_PATH, ImportCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidFileExtension_throwsParseException() {
+        assertParseFailure(parser, "testing.txt", String.format(MESSAGE_INVALID_FILE_EXTENSION, ImportCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_csvFileFormat_success() {
+        String filename = "testing.csv";
+        ImportCommand importCommand = new ImportCommand(new CsvFile(filename), new CsvConverter());
+        assertParseSuccess(parser, filename, importCommand);
+    }
+}
