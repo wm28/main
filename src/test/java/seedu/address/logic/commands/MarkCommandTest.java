79f9ed3b12741aafe6d31ecd875817225dd79639
kronicler <omnikron96@gmail.com>
2018-11-01 14:31:47 +0800

    Added initial tests and updating documentation

---
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -0,0 +1,72 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_DIET_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.MarkCommand.EditPersonDescriptor;
+import seedu.address.logic.commands.MarkCommand;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.EditPersonDescriptorBuilder;
+import seedu.address.testutil.PersonBuilder;
+
+//@@author kronicler
+class MarkCommandTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void constructor_nullPerson_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new MarkCommand(null);
+    }
+
+    @Test
+    void retrieveIndex_noPersonInGuestList_throwCommandException() {
+
+    }
+
+    @Test
+    void retrieveIndex_filteredGuestListWithoutThePhoneNumber_throwCommandException() {
+    }
+
+    @Test
+    void retrieveIndex_filteredGuestListWithThePhoneNumber_success() {
+    }
+
+    @Test
+    void retrieveIndex_unfilteredGuestListWithThePhoneNumber_success() {
+    }
+
+    @Test
+    void execute_phoneNumberExistsInGuestList_success() {
+    }
+
+    @Test
+    void execute_phoneNumberDoesNotExistInGuestList_throwCommandException() {
+    }
+
+}
