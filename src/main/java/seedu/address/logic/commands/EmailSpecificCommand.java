bb36366c1ee35b0edf12513a93afb0dca1c5b8c7
aaryamNUS <aaryamps1@gmail.com>
2018-11-06 16:18:24 +0800

    Refactored Email code

--- src/main/java/seedu/address/logic/commands/EmailSpecificCommand.java
+++ src/main/java/seedu/address/logic/commands/EmailSpecificCommand.java
@@ -41,6 +41,8 @@ public class EmailSpecificCommand extends Email {
     private static Logger logger = Logger.getLogger("execute");
     private static String username;
     private static String password;
+    private static String emailSubject;
+    private static String emailMessage;
     private final Set<Tag> tagsToSend;
 
     /**
@@ -59,63 +61,29 @@ public class EmailSpecificCommand extends Email {
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
-        String emailSubject;
-        String emailMessage;
 
         // Check for duplicate emails to ensure each guest only receives one email, even if
         // multiple guests have registered under the same email
-        HashSet<String> personsToSendEmail = new HashSet<>();
+        HashSet<String> personsToSendEmail = createMailingList(lastShownList, tagsToSend);
 
-        // Used to build the final recipients String
-        StringBuilder personsToEmail = new StringBuilder();
-        String recipients;
-
-        // First check which of the guests have at least one of the specified tags,
-        // and filter out any duplicate guests
-        for (Person individualGuests : lastShownList) {
-            for (Tag singleTags : tagsToSend) {
-                if (individualGuests.getTags().contains(singleTags)) {
-                    if (personsToSendEmail.contains(individualGuests.getEmail().toString())) {
-                        logger.log(Level.INFO, "Guest email address has already been sent an email!");
-                    } else {
-                        personsToSendEmail.add(individualGuests.getEmail().toString());
-                    }
-                }
-            }
-        }
-
-        // If no guests have the specified tags, throw a Command Exception
-        if (personsToSendEmail.isEmpty()) {
-            throw new CommandException(Messages.MESSAGE_NO_PERSONS_WITH_TAGS);
-        }
-
-        // Create a string with all the recipients
-        for (String personToEmail : personsToSendEmail) {
-            String individualGuest = personToEmail + ",";
-            personsToEmail.append(individualGuest);
-        }
-
-        recipients = removeLastChar(personsToEmail.toString());
-
-        // Array of strings to store all the necessary information
-        String[] information;
-        // Retrieve the information through a method in the super class Email
-        information = retrieveInformation();
+        // Strings to represent the recipients of the email (i.e. all the guests in the list)
+        String recipients = recipientCreator(personsToSendEmail);
 
         try {
-            username = information[0];
-            password = information[1];
-            emailSubject = information[2];
-            emailMessage = information[3];
+            // Array of strings to store all the necessary information
+            String[] information;
+            // Retrieve the information through a method in the super class Email
+            information = retrieveInformation();
+            setInformation(information);
 
-            // Creates a new session with the user gmail account as the host
+            // Creates a new session with the user Gmail account as the host
             Properties props = createPropertiesConfiguration();
 
             // Authenticate the user credentials
             EmailPasswordAuthenticator authenticate = new EmailPasswordAuthenticator();
 
             // Create a new session using the authenticated credentials and the properties of
-            // the gmail host
+            // the Gmail host
             Session session = Session.getDefaultInstance(props, authenticate);
 
             createAndSendEmail(username, emailSubject, emailMessage,
@@ -130,13 +98,47 @@ public class EmailSpecificCommand extends Email {
     }
 
     /**
-     * Removes the last character out of the recipients String as it contains
-     * an unwanted ',' character
-     * @param string is the original recipients string
-     * @return the substring
+     * Sets the information retrieved from an EmailWindow
      */
-    private static String removeLastChar(String string) {
-        return string.substring(0, string.length() - 1);
+    private static void setInformation(String[] information) {
+        username = information[0];
+        password = information[1];
+        emailSubject = information[2];
+        emailMessage = information[3];
+    }
+
+    /**
+     * Create a mailing list with all the email addresses of guests, this method
+     * also filters out duplicate or invalid email addresses
+     * @param lastShownList the last known guest list
+     * @param tagsToSend list of tags to match with guests
+     * @return the email list
+     */
+    private HashSet<String> createMailingList(List<Person> lastShownList, Set<Tag> tagsToSend) throws CommandException {
+        HashSet<String> personsToSendEmail = new HashSet<>();
+
+        // First check which of the guests have at least one of the specified tags,
+        // and filter out any duplicate guests
+        for (Person individualGuests : lastShownList) {
+            for (Tag singleTags : tagsToSend) {
+                if (individualGuests.getTags().contains(singleTags)) {
+                    if (personsToSendEmail.contains(individualGuests.getEmail().toString())) {
+                        logger.log(Level.INFO, "Guest email address has already been sent an email!");
+                    } else if (!isValidEmail(individualGuests.getEmail().toString())) {
+                        logger.log(Level.INFO, "Guest email address is not valid!");
+                    } else {
+                        personsToSendEmail.add(individualGuests.getEmail().toString());
+                    }
+                }
+            }
+        }
+
+        // If no guests have the specified tags, throw a Command Exception
+        if (personsToSendEmail.isEmpty()) {
+            throw new CommandException(Messages.MESSAGE_NO_PERSONS_WITH_TAGS);
+        }
+
+        return personsToSendEmail;
     }
 
     @Override
@@ -147,6 +149,11 @@ public class EmailSpecificCommand extends Email {
     @Override
     public String[] retrieveInformation() throws CommandException {
         return super.retrieveInformation();
+    }
+
+    @Override
+    public String recipientCreator(HashSet<String> personsToSendEmail) {
+        return super.recipientCreator(personsToSendEmail);
     }
 
     /**
